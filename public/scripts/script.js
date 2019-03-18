3de0cbd520c6e3931db65ba28f3afa7b1eefea03
David Langham <dlangham@users.noreply.github.com>
2019-03-18 15:21:40 -0500

    iterate & debug mobile menu

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,41 +1,83 @@
+const w = window;
 const d = document;
+
+//breakpoints
+const mobileAndDown = 767.98;
+const tabletDesktopAndUp = 768;
 
 //mobile hamburger icon////////////////////////////////////
 
 const hambIcon = d.querySelector('#hamb-icon');
 const mobileLinks = d.querySelector('#mobile-menu .nav-links');
 const brand = d.querySelector('.brand');
-const topA = d.querySelector('.top-a');
-const topB = d.querySelectorAll('.top-b');
+const offMenu = d.querySelector('.off-menu');
 const body = d.querySelector('body');
-const page = d.querySelector('.page');
-var isPageListener = false;
+var isMenuOpen = false;
+var isResizeListener = false;
+var isOffMenuListener = false;
 
-const onClickHambIcon = (fn) => {
+const uponClickHambIcon = (fn) => {
     hambIcon.addEventListener('click', fn);
 };
-const onClickPage = (fn) => {
-    page.addEventListener('click', fn);
-    isPageListener = true;
+const uponClickOffMenu = (fn) => {
+    offMenu.addEventListener('click', fn);
+    isOffMenuListener = true;
+};
+const removeUponClickOffMenu = (fn) => {
+    offMenu.removeEventListener('click', fn);
+    isOffMenuListener = false;
+};
+const uponResize = (fn) => {
+    w.addEventListener('resize', fn);
+    isResizeListener = true;
+};
+const removeUponResize = (fn) => {
+    w.removeEventListener('resize', fn);
+    isResizeListener = false;
+};
+const uponBreakpoint = () => {
+    //if hit breakpoint and menu is open
+    if (w.innerWidth >= tabletDesktopAndUp && isMenuOpen) {
+        //close menu and remove listener
+        toggleMobileMenu();
+    }
 };
 
 const toggleMobileMenu = () => {
+    // console.log('click hambIcon, so toggleMobileMenu');
+    
     hambIcon.classList.toggle('open');
     mobileLinks.classList.toggle('hidden');
     brand.classList.toggle('hidden');
     // body.classList.toggle('no-scroll');
 
-    //toggle off .page listener
-    if (isPageListener === true) {
-        page.removeEventListener('click', toggleMobileMenu);
-        isPageListener = false;
+    console.log(isMenuOpen ? 'now closing menu' : 'now opening menu');
+
+    //toggle isMenuOpen
+    isMenuOpen = isMenuOpen ? false : true;
+    
+
+    //if menu is open
+    if (isMenuOpen) {
+        //listen for window resize; is it past breakpoint? then close menu
+        uponResize(uponBreakpoint);
+    } else {
+        //remove listener for resize
+        if (isResizeListener) {
+            removeUponResize(toggleMobileMenu);
+            console.log('removed "resize" listener, in "if isMenuOpen is false"');
+        }
+        
     }
-    //toggle on .page listener
-    if (!mobileLinks.classList.contains('hidden')) {
-        onClickPage(toggleMobileMenu);
-    }
+    
+    // // toggle on .off-menu listener
+    // if (isOffMenuListener === false) uponClickOffMenu(toggleMobileMenu);
+    // //toggle off .off-menu listener
+    // else if (isOffMenuListener === true) removeUponResize(toggleMobileMenu);
+    isOffMenuListener === true ? removeUponClickOffMenu(toggleMobileMenu) : uponClickOffMenu(toggleMobileMenu);
 }
 
 d.addEventListener('DOMContentLoaded', () => {
-    onClickHambIcon(toggleMobileMenu);
+    uponClickHambIcon(toggleMobileMenu);
 });
+
